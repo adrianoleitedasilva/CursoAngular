0c267be13f59ec8ac4eca054314f07eef80e33f7
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2023-08-07 21:20:12 -0300

    update

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,17 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-title title="Adriano"></app-title>
+
+  <app-title *ngIf="destruir"></app-title>
+  <br>
+  <button (click)="destruirComponente()">Destruir componente</button>
   <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent implements OnInit {
+  
+  public destruir:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
     setTimeout(()=> {
       console.log(1)
     }, 5000)
+  }
+
+  public destruirComponente() {
+    this.destruir = false;
   }
 }
--- meu-primeiro-projeto/src/app/title/title.component.ts
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input} from '@angular/core';
+import { Component, OnChanges, Input, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,10 +6,14 @@ import { Component, OnInit, OnChanges, Input} from '@angular/core';
   styleUrls: ['./title.component.scss']
 })
 
-export class TitleComponent implements OnChanges {
+export class TitleComponent implements OnChanges, OnDestroy {
   @Input() title: string = 'Titulo'
 
   ngOnChanges():void {
     console.log("Foi alterado com sucesso!")
+  }
+
+  ngOnDestroy(): void {
+    console.log('Deu bom, destruiu!');
   }
 }
